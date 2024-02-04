1f3a41017e74d82e9fd2e960097bf1f39fa99df7
Danny Nguyen <nguyenduongpa@gmail.com>
2024-02-04 12:19:45 -0800

    added two forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    login_form = AuthenticationForm()
+    register_form = UserCreationForm()
+    context = {'register_form': register_form, 'login_form': login_form}
+    return render(request, 'index.html', context)
