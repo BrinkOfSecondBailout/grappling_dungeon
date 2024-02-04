723300cb7aef9a4d9189ba0cec9c11c4035dc56a
Danny Nguyen <nguyenduongpa@gmail.com>
2024-02-04 13:32:18 -0800

    trying to figure out login and auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 # Create your views here.
 def index(request):
-    login_form = AuthenticationForm()
-    register_form = UserCreationForm()
+    login_form = CustomAuthenticationForm()
+    register_form = CustomUserCreationForm()
     context = {'register_form': register_form, 'login_form': login_form}
     return render(request, 'index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        register_form = CustomUserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            return redirect('dashboard')
+        else:
+            return redirect('login')
+
+    register_form = CustomUserCreationForm()
+    context = {'register_form': register_form}
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        login_form = CustomAuthenticationForm(request.POST)
+        if login_form.is_valid():
+            return redirect('dashboard')
+        else:
+            return redirect('login')
+        
+    login_form = CustomAuthenticationForm()
+    context = {'login_form': login_form}
+    return render(request, 'login.html', context)
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
