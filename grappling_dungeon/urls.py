fc3ee482a5645f5e3b8d66f72e96aa1837d3846b
Danny Nguyen <nguyenduongpa@gmail.com>
2024-02-05 17:28:42 -0800

    fixed links

--- grappling_dungeon/urls.py
+++ grappling_dungeon/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('techniques.urls')),
+    path('techniques/', include('techniques.urls')),
 ]
