729e9fbb0aacd75c143f0db544ea3307d52c288f
Danny Nguyen <nguyenduongpa@gmail.com>
2024-02-06 11:03:04 -0800

    working on models

--- techniques/models.py
+++ techniques/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Technique(models.Model):
@@ -16,10 +16,22 @@ class Technique(models.Model):
         ('public', 'Public'),
     ]
 
+    VIDEO_CHOICES = [
+        ('full', 'Full Youtube Video'),
+        ('cropped', 'Crop Short Clip from Youtube Video'),
+    ]
+
+    user_model = settings.AUTH_USER_MODEL
+
     name = models.CharField(max_length = 255)
     category = models.CharField(max_length = 20, choices=CATEGORY_CHOICES)
     privacy_status = models.CharField(max_length=20, choices=PRIVACY_CHOICES)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    video_option = models.CharField(max_length = 10, choices=VIDEO_CHOICES)
+    youtube_url = models.URLField()
+
+    start_time = models.PositiveIntegerField(blank=True, null=True)
+    end_time = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
