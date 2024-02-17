393f76a1bec4f0f7dc051e9da8262bbbb68a1337
Danny Nguyen <nguyenduongpa@gmail.com>
2024-02-17 11:30:45 -0800

    adding technique working

--- playlists/urls.py
+++ playlists/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('playlist/', views.find_playlist, name='find_playlist'),
-    path('add', views.add_to_playlist, name='add_to_playlist'),
+    path('add/', views.add_to_playlist, name='add_to_playlist'),
 ]
--- playlists/views.py
+++ playlists/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .forms import PlaylistForm
+from .models import Playlist, PlaylistItem, Technique
 
 # Create your views here.
 
@@ -19,5 +21,35 @@ def find_playlist(request):
 @login_required
 def add_to_playlist(request):
     if request.method == 'POST':
-        print(request.POST)
-    return redirect('private')
+        user = request.user
+        playlist_name = request.POST.get('name')
+        technique_id = request.POST.get('technique')
+        print(playlist_name)
+        print(technique_id)
+
+        playlist = Playlist.objects.filter(owner=user, name=playlist_name).first()
+        technique = get_object_or_404(Technique, id=technique_id)
+
+        if playlist:
+            order = PlaylistItem.objects.filter(playlist=playlist).count() + 1
+
+            playlist_item = PlaylistItem(playlist=playlist, technique=technique, order=order)
+            playlist_item.save()
+
+            print(f'Technique added to existing playlist: {playlist_name}')
+
+        else:
+            new_playlist = Playlist(owner=user, name=playlist_name)
+            new_playlist.save()
+
+            order = 1
+
+            playlist_item = PlaylistItem(playlist=new_playlist, technique=technique, order=order)
+            playlist_item.save()
+
+            print(f'Technique added to new playlist: {playlist_name}')
+
+        return redirect('private')
+
+    else:
+        pass
