2c6a309bdd9deb01c5dd55665834be196baed8d6
Danny Nguyen <nguyenduongpa@gmail.com>
2024-03-12 09:06:15 -0700

    inbox message

--- messages/models.py
+++ messages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from accounts.models import User
 # Create your models here.
 
 class Inbox(models.Model):
@@ -15,10 +15,17 @@ class Inbox(models.Model):
     
 class Thread(models.Model):
     inbox = models.ForeignKey(Inbox, on_delete=models.CASCADE, related_name='threads')
+    correspondence = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return f"Thread in {self.inbox.owner.name}'s Inbox"
+        return f"Threads in {self.inbox.owner.name}'s Inbox"
 
 class Message(models.Model):
     content = models.TextField(max_length=1000)
-    
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='messages')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Messages in {self.thread.inbox.owner.name}'s Inbox"
