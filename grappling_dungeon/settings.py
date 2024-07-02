aa3afaac612166fa4ccfca603ca1a0011611b21c
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-02 09:02:32 -0700

    switched to sqllite

--- grappling_dungeon/settings.py
+++ grappling_dungeon/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'grappling_dungeon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'grappling_dungeon',
-        'USER': 'postgres',
-        'PASSWORD': 'DannyDev89',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'grappling_dungeon',
+#         'USER': 'postgres',
+#         'PASSWORD': 'DannyDev89',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'accounts', 'static'),
